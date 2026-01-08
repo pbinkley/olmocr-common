8b6c8a17d2ce52030d6f00256eae4c8d97395157
Peter Binkley <peter.binkley@ualberta.ca>
2026-01-08 16:45:52 -0700

    Expand system info presented by check_setup.py

--- check_setup.py
+++ check_setup.py
@@ -1,21 +1,40 @@
 import sys
 import torch
+from importlib.metadata import version
+import platform
+import psutil
 
 print(f"Python version: {sys.version}")
+print(f"Torch version: {torch.__version__}")
+print(f"System: {platform.system()}; {round(psutil.virtual_memory().total/1024**3, 2)}GB memory")
 
 try:
     import olmocr
-    print("✅ olmocr: Found")
+    print(f"✅ olmocr: Found {version('olmocr')}")
 except ImportError:
     print("❌ olmocr: Not Found")
 
 try:
     import mlx.core as mx
     import mlx_vlm
-    print(f"✅ MLX: Found (Device: {mx.default_device()})")
+    print(f"✅ MLX Available (Device: {mx.default_device()})")
     print("✅ mlx-vlm: Found")
 except ImportError as e:
-    print(f"❌ MLX Error: {e}")
+    print(f"❌ MLX: Not Available")
 
 if torch.backends.mps.is_available():
-    print("✅ Metal Performance Shaders (MPS): Available")
+    print("✅ MPS: Available")
+else:
+    print("❌ MPS: Not Available")    
+
+if torch.cuda.is_available():
+    cuda = torch.cuda.get_device_properties(0) # TODO handle multiple GPUs
+    print(f"✅ CUDA: Available: {cuda['name']} ({cuda['major']}.{cuda['minor']}, {cuda['total_memory']})")
+else:
+    print("❌ CUDA: Not available")
+
+if torch.cpu.is_available():
+    print(f"✅ CPU: Available: {platform.processor()}")
+else:
+    print("❌ CPU: Not available")
+
